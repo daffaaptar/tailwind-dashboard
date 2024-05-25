d98ae9a1b1a4fc09186c7fb3dadd96da14d4003b
daffaaptar <daffaaptapratama@gmail.com>
2024-05-25 14:47:26 +0700

    commit 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ['./public/**/*.{html,js}'],
+  content: ['./index.html'],
   theme: {
     container : {
       center: true,
